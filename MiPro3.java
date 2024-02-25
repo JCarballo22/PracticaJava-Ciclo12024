2061a7af57f6a0a2064b6930a1969e6ea438bb5c
Jonathan Carballo <110014671+JCarballo22@users.noreply.github.com>
2024-02-25 12:00:08 -0600

    S05

---
+++ MiPro3.java
@@ -0,0 +1,96 @@
+
+
+package sv.edu.unab.mipro3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Carba
+ */
+public class MiPro3 {
+
+    public static void main(String[] args) {
+       /* String abecedario = "abcde fghijk lmnopqr stuvwxyz";
+        System.out.println("La longitud de la variable es: " + abecedario.length());
+        
+        String txt = "Elementos Básicos de Java";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+        
+       String cadena = "La 1 programación no 1 es dificil 1 si te esmeras";
+       Scanner entrada = new Scanner(System.in);
+       String buscar = entrada.nextLine();
+       System.out.println(cadena.indexOf(buscar));
+      
+       String nombre = "Vladimir";
+       String apellido = "Villalobos";
+       
+       System.out.println(nombre +" "+ apellido);
+       System.out.println(nombre.concat(" "+apellido));
+       
+       int x = 10;
+       int y = 20;
+       int z = x + y; // 30
+        System.out.println(z);
+       
+       String x1 = "10";
+       String y1 = "20";
+       String z1 = x1 + y1;
+       System.out.println(z1);
+       
+       String x2 = "10";
+       int y2 = 20;
+       String z2 = x2+y2;
+       System.out.println(z2);
+       
+       String texto = "La ciencia de hoy es la \"tecnología\" del mañana";
+       String texto1 = "It\'s my \\brother";
+       System.out.println(texto1);
+       
+       String texto = "Hola\nestudiantes";
+       String texto1 = "Hola\restudiantes";
+       String texto2 = "Hola\testudiantes";
+       String texto3 = "Ho\blaestudiantes";
+        System.out.println(texto);
+        System.out.println(texto1);
+        System.out.println(texto2);
+        System.out.println(texto3);
+        
+        System.out.println(Math.max(6,10));
+        System.out.println(Math.min(6,10));
+        System.out.println(Math.sqrt(73));
+        System.out.println(Math.abs(-9.5));
+        System.out.println(Math.PI);
+        System.out.println(Math.random());
+        
+        int randomNum = (int)(Math.random()*100);
+        System.out.println(randomNum);
+        
+        boolean EsVerdad = true;
+        boolean EsMentira = false;
+        System.out.println(EsVerdad);
+        System.out.println(EsMentira);
+        
+        int x = 6;
+        int y = 5;
+        System.out.println(x>y);
+        
+        int z = 3;
+        System.out.println(z==3);*/
+        
+        int a = 11;
+        int b = 11;
+        if (a>b){
+            System.out.println("a es mayor que b");
+        }
+        
+        int c = 16;
+        if (c>=18){
+            System.out.println("Eres mayor de edad y tu edad es: " +c );
+        }else{
+            System.out.println("Eres menor de edad y tu edad es: "+c);
+        }
+        
+    }
+}
